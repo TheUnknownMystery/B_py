9f09fb00bfd6adacbdeb8b4e0822dede2a4f7124
SauhardoSengupta <70318077+sauhardo2020@users.noreply.github.com>
2021-08-12 15:25:22 +0530

    Done
    Done

---
+++ Background.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import time
+
+video = cv2.VideoCapture(0)
+
+img = cv2.imread("BackGround\image.jpg")
+
+while True:
+    ret, frame = video.read()
+    frame = cv2.resize(frame, (640, 480))
+    img = cv2.resize(img, (640, 480))
+
+    upper_black = np.array([104, 153, 70])
+    lower_black = np.array([30, 30, 0])
+
+    mask = cv2.inRange(frame, lower_black, upper_black)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    f = frame - res
+    f = np.where(f == 0, img, f)
+    cv2.imshow("Video", frame)
+    cv2.imshow("Vide_masked", f)
+
+    if(cv2.waitKey(1) & 0xFF == ord("q")):
+        break
+
+video.release()
+cv2.destroyAllWindows()
